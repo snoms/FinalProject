120fb5e8b74000b0396345ed4ff8d433378767b7
snoms <tweaks@gmail.com>
2016-06-07 13:44:05 +0200

    first commit

---
+++ Trip/Trip/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Trip
+//
+//  Created by bob on 07/06/16.
+//  Copyright © 2016 bob. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

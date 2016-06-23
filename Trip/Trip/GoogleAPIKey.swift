ff1ec69faab64f8293ce34e968a810cd7d429077
snoms <tweaks@gmail.com>
2016-06-23 17:04:08 +0200

    google API key added back in

---
+++ Trip/Trip/GoogleAPIKey.swift
@@ -0,0 +1,16 @@
+//
+//  GoogleAPIKey.swift
+//  Trip
+//
+//  Created by bob on 07/06/16.
+//  Copyright © 2016 bob. All rights reserved.
+//
+//  Declare Google Developers API key (not committed to Git)
+
+import Foundation
+
+let GoogleAPIkey = "AIzaSyAeGSCh48QkeVuEJca5FhuU34jPUQ6BWYA"
+
+// Alternative backup key
+
+//AIzaSyD2AyUNpejs73Z5verBm87hp6VnTgCiIN4
